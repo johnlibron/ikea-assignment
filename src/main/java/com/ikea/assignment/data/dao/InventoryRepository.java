80f5dd516683b1fdaa262c272c650bdd11c6e2fc
John James Libron <johnjames.libron@gmail.com>
2022-07-31 21:58:36 +0800

    Change String to Long

--- src/main/java/com/ikea/assignment/data/dao/InventoryRepository.java
+++ src/main/java/com/ikea/assignment/data/dao/InventoryRepository.java
@@ -3,5 +3,5 @@ package com.ikea.assignment.data.dao;
 import com.ikea.assignment.data.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InventoryRepository extends JpaRepository<Inventory, String> {
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 }
